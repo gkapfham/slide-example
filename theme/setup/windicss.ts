b004f91f28030af11fa555a7e9dd202ecf5b8011
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2021-08-20 15:32:45 -0400

    Add theme configuration details to the windicss.ts file.

--- theme/setup/windicss.ts
+++ theme/setup/windicss.ts
@@ -13,4 +13,17 @@ export default defineWindiSetup(() => ({
   shortcuts: {
     'bg-main': 'bg-white text-[#181818] dark:(bg-[#121212] text-[#ddd])',
   },
+    theme: {
+      extend: {
+        fontFamily: {
+          sans: '"IBM Plex Sans","Nunito Sans",ui-sans-serif,system-ui,-apple-system,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,"Noto Sans",sans-serif,"Apple Color Emoji","Segoe UI Emoji","Segoe UI Symbol","Noto Color Emoji"',
+          mono: '"IBM Plex Mono", monospace',
+        },
+        colors: {
+          primary: {
+            DEFAULT: '#42b883',
+          },
+        },
+      },
+    },
 }))
